a5c560f758aea08d411b56036115ff560944400e
gaozhangyang <2794485326@qq.com>
2021-11-04 01:12:44 +0000

    the virsion of AAAI22

--- git_cluster/detect_local_mode.py
+++ git_cluster/detect_local_mode.py
@@ -1,60 +1,62 @@
 import numpy as np
-from .kde import KDE_DIS
-
-
+from kde import KDE_DIS
+import time
+        
 class LCluster:
-    def __init__(
-        self, 
-        K_d, 
-        K_s, 
-        scale
-    ):
-        self.K_d = K_d
-        self.K_s = K_s
-        self.scale = scale
+    def __init__(self):
+        pass
 
-    def detect_descending_manifolds(self, X):
-        extend = np.arange(0, X.shape[0]).reshape(-1, 1).repeat(4, axis=1)
-        X_extend = np.hstack([X, extend])
+    @classmethod
+    def detect_descending_manifolds(self,X,K_d,K_s):
+        extend=np.arange(0,X.shape[0]).reshape(-1,1).repeat(4,axis=1) # extend 4 dim, representing $density $ID $local clusters $ label
+        X_extend=np.hstack([X,extend])
 
-        Dis = KDE_DIS(X_extend[:, :-4], K=self.K_d, scale=self.scale)
-        D, I = Dis.get_DI(X_extend[:, -3])
-        P = Dis.get_density(X_extend[:, -3])
-        X_extend[:, -4] = P
+        t0=time.time()
+        Dis=KDE_DIS(X_extend[:,:-4],K=K_d)
+        D,I = Dis.get_DI(X_extend[:,-3])
+        P = Dis.get_density(X_extend[:,-3])
+        t2=time.time()
+        X_extend[:,-4]=P
 
-        idx = np.argsort(P).tolist()
+        idx=np.argsort(P).tolist()
         idx.reverse()
-        Boundary = []
+        Boundary=[]
 
         Back_mask = np.ones(X.shape[0])
-        index = 0
+        draw_step=int(X.shape[0]//100)
+        index=0
+        draw_tasks=[]
 
-        while len(idx) > 0:
-            i = idx[0]  # Get the point with the highest current density, i
-            index += 1
-            Back_mask[i] = 0
+        
+        while len(idx)>0:
+            i=idx[0] # Get the point with the highest current density, i
+            index+=1
+            Back_mask[i]=0
 
-            idx_iN = I[i, 1:self.K_s+1]  # the index value nearest k neighbors
-            mask = P[idx_iN] > P[i]
-            J = idx_iN[mask]
+            idx_iN=I[i,1:K_s+1] # the index value nearest k neighbors
+            mask = P[idx_iN]>P[i]
+            J=idx_iN[mask] # the density of the selected neighboring points is greater than the current point
 
-            if J.shape[0] > 0:
-                grad = (P[J]-P[i])/(D[i, 1:self.K_s+1][mask])
-                j = J[np.argmax(grad)]
-            else:  j = None  # The father node was not found
+            if J.shape[0]>0:
+                grad=(P[J]-P[i])/(D[i,1:K_s+1][mask])
+                j=J[np.argmax(grad)]
+            else:
+                j=None # The father node was not found
 
-            if j is not None:  # The father node was found
-                X_extend[i, -2] = X_extend[j, -2]
+            if j is not None: # The father node was found
+                X_extend[i,-2]=X_extend[j,-2]
                 for s in J[1:]:
-                    if X_extend[s, -2] != X_extend[i, -2]:
-                        Boundary.append(
-                            (i, s, int(X_extend[i, -2]), int(X_extend[s, -2])))
-
-            idx.remove(i)  # delete point i
-
-        R = X_extend[:, -2]
-        V = {int(j): [] for j in set(R)}
+                    if X_extend[s,-2]!=X_extend[i,-2]:
+                        Boundary.append((i,s,int(X_extend[i,-2]),int(X_extend[s,-2])))
+            
+            if index%draw_step==0:
+                draw_tasks.append( (X[:,:2],X[:,-2],Back_mask.copy(),index) ) # points, local clusters, Back_mask, time index
+            
+            idx.remove(i) # delete point i
+        
+        R=X_extend[:,-2]
+        V={int(j):[] for j in set(R)}
         for i in range(len(R)):
             V[R[i]].append(i)
-
-        return V, Boundary, X_extend, Dis
+        
+        return V,Boundary,X_extend,Dis,draw_tasks,t2
--- git_cluster/topo_graph.py
+++ git_cluster/topo_graph.py
@@ -1,55 +1,168 @@
 import numpy as np
+from scipy.optimize import linprog
+
+PCA_MNIST_GIT = [0.478355, 0.691515, 0.77153, 0.599865,0.551863]
+PCA_MNIST_KM = [0.419655,0.498879,0.467717,0.501209,0.503281]
+PCA_MNIST_SA = [0.305144,0.390322,0.36481,0.388679,0.399094]
+PCA_MNIST_Q = [0.245804,0.646372,0.486525,0.541622,0.45017]
+
+
+PCA_FMNIST_GIT = [0.592528,0.686799,0.643216,0.537525,0.551863]
+PCA_FMNIST_KM = [0.492665,0.515958,0.387934,0.3879,0.388148]
+PCA_FMNIST_SA = [0.435895,0.469618,0.419684,0.467485,0.473277]
+PCA_FMNIST_Q = [0.422668,0.424779,0.402471,0.382822,0.41524]
+
+
+AE_MNIST_GIT = [0.878381,0.861547,0.880494,0.842948,0.520227]
+AE_MNIST_KM = [0.721353,0.775986,0.519486,0.485933,0.503281]
+AE_MNIST_SA = [0.654951,0.621669,0.501961,0.424814,0.399094]
+AE_MNIST_Q = [0.779808,0.739898,0.785197,0.680408,0.45017]
+
+
+AE_FMNIST_GIT = [0.647393,0.578947,0.577126,0.650498,0.520227]
+AE_FMNIST_KM = [0.39582,0.546557,0.527729,0.503655,0.388148]
+AE_FMNIST_SA = [0.470541,0.493084,0.477636,0.378715,0.473277]
+AE_FMNIST_Q = [0.345888,0.4461,0.384507,0.370655,0.41524]
+
+
+def wasserstein_distance(p, q):
+    """通过线性规划求Wasserstein距离
+    p.shape=[m], q.shape=[n], D.shape=[m, n]
+    p.sum()=1, q.sum()=1, p∈[0,1], q∈[0,1]
+    """
+    p = p/p.sum()
+    q = q/q.sum()
+    D = np.sqrt(np.square(p[:, None] - q[None, :]))
+    # D = np.ones((p.shape[0],q.shape[0]))
+
+    A_eq = []
+    for i in range(len(p)):
+        A = np.zeros_like(D)
+        A[i, :] = 1
+        A_eq.append(A.reshape(-1))
+    for i in range(len(q)):
+        A = np.zeros_like(D)
+        A[:, i] = 1
+        A_eq.append(A.reshape(-1))
+    A_eq = np.array(A_eq)
+    b_eq = np.concatenate([p, q])
+    D = D.reshape(-1)
+    result = linprog(D, A_eq=A_eq[:-1], b_eq=b_eq[:-1])
+    return result.fun
 
 
 class TopoGraph:
-    def __init__(
-        self,
-        alpha,
-    ):
-        self.alpha = alpha
+    def __init__(self):
+        pass
 
-    def topograph_construction_pruning(self, V, X_extend, Boundary, Dis):
-        E, E_hat, gamma, count = {}, {}, {}, {}
-        X_mid = []
-        for (i, j, ri, rj) in Boundary:
-            E[(ri, rj)], E[(rj, ri)]  = 0, 0
-            E_hat[ri], E_hat[rj] = 0, 0
-            gamma[(ri, rj)] = None
-            count[(ri, rj)] = 0
-            X_mid.append((X_extend[i, -3], X_extend[j, -3]))
+    def graph_construction(self,V,X_extend,Boundary,Dis):
+        E={}
+        X_dis=[]
+        for (i,j,ri,rj) in Boundary:
+            E[(ri,rj)]=0
+            E[(rj,ri)]=0
+            X_dis.append( np.linalg.norm(X_extend[i,:-4]-X_extend[j,:-4]) )
 
-        if len(X_mid) == 0:
-            return E.copy(), E
+        if len(X_dis)==0:
+            RT=list(V.keys())
+            return {(RT[0],RT[0]):0}
 
-        X_mid = np.array(X_mid).astype(np.int)
-        P_mid = Dis.get_midP(X_mid[:, 0], X_mid[:, 1])
+        for idx,(i,j,ri,rj) in enumerate(Boundary):
+            s= np.exp(-X_dis[idx])/(len(V[ri])*len(V[rj]))
+            E[(ri,rj)]+=s
+            E[(rj,ri)]=E[(ri,rj)]
+        return E
 
-        for idx, (i, j, ri, rj) in enumerate(Boundary):
-            if gamma[(ri, rj)] is None:
-                P1, P2 = X_extend[ri, -4], X_extend[rj, -4]
-                gamma[(ri, rj)] = gamma[(rj, ri)] = min(
-                    P1 / P2, P2 / P1)**2
+    def graph_pruning(self,E_raw,V,target,DataNumber):
+        # TODO 当E_raw为空时，报错
+        # print(E_raw)
+        edges=[(i,j,v) for (i,j),v in E_raw.items()]
+        edges=sorted(edges,key=lambda x:x[2],reverse=True)
+        C=[{i:i for i in V.keys()}] # node_idx: cls_idx
+        M=[{i:[i] for i in V.keys()}] # cls_idx: [node_idx]
+        N=[{i:len(V[i]) for i in V.keys()}] # number of points in each cluster
+        score=[9999]
+        c=len(target)
+        target = np.array(target)/np.array(target).sum()*DataNumber
+        useless_edges = []
+        E_final={}
 
-            s = (gamma[(ri, rj)]*(P_mid[idx])**2) / (len(V[ri])*len(V[rj]))
-            count[(ri, rj)] += 1
-            count[(rj, ri)] = count[(ri, rj)]
-            E[(ri, rj)] += s
-            E[(rj, ri)] = E[(ri, rj)]
+        for i,j,v in edges:
+            C_prev=C[-1]
+            M_prev=M[-1]
+            N_prev=N[-1]
+            if C_prev[i]!=C_prev[j]:
+                S_i=M_prev[i]
+                S_j=M_prev[j]
+                S=S_i+S_j
+                
+                C_now=C_prev.copy()
+                for k in S_j:
+                    C_now[k]=C_prev[i]
+                
+                M_now=M_prev.copy()
+                for k in S:
+                    M_now[k]=S
+                    
+                N_now=N_prev.copy()
+                for k in S:
+                    N_now[k]=N_prev[i]+N_prev[j]
+                
+                seq = np.array(sorted([N_now[c] for c in set(C_now.values())],reverse=True))
+                if len(seq)<c:
+                    break
+                # sc = word_mover_distance(seq[:c],target)
+                # sc = wasserstein_distance(seq[:c],target)
+                sc = wasserstein_distance(seq,target)
+                if sc<=score[-1]:
+                    score.append(sc)
+                    C.append(C_now)
+                    M.append(M_now)
+                    N.append(N_now)
+                    E_final[(i,j)]=v
+                else:
+                    useless_edges.append((i,j,v))
+        
+        cls_set = list(set(C[-1].values()))
+        classes = {idx:M[-1][root_idx] for idx,root_idx in enumerate(cls_set) } # ordered number-->index of local clusters in the same classes, e.g., 1:[2658, 2691, 2579, 2556, 2258, 2645]
+        
+        point_numbers = {idx:N[-1][root_idx] for idx,root_idx in enumerate(cls_set) }
+        root2order = { root_idx:idx for idx,root_idx in enumerate(cls_set) }
+        
+        A = np.zeros((len(cls_set),len(cls_set)))
+        for i,j,v in useless_edges:
+            row = root2order[C[-1][i]]
+            col = root2order[C[-1][j]]
+            A[row,col]+=v
+            A[col,row]+=v
+        
+        N_pred = np.array(list(point_numbers.values()))
+        merge_idx = np.argsort(N_pred)
+        target_set = merge_idx[-c:].tolist()
+        source_set = merge_idx[:-c].tolist()
+        A[:,target_set]=0
+        target_idx, source_idx = A.nonzero()
+        edges = [[i,j,A[i,j]] for i in target_idx for j in source_idx]
+        edges = sorted(edges,key=lambda x: x[2],reverse=True)
 
-            if E[(ri, rj)] > E_hat[ri]:  E_hat[ri] = E[(ri, rj)]
-            if E[(ri, rj)] > E_hat[rj]:  E_hat[rj] = E[(ri, rj)]
+        re_direct = {i:i for i in range(A.shape[0])}
+        for target, source, value in edges:
+            if len(source_set)==0:
+                break
 
-        E_raw = E.copy()
-        for key, value in E.items():
-            i, j = key[0], key[1]
-            if value <= self.alpha*E_hat[i] or value <= self.alpha*E_hat[j]:
-                E[(i, j)], E[(j, i)] = 0, 0
+            if source in source_set:
+                if re_direct[target]!=source:
+                    classes[re_direct[target]]+=classes[source]
+                    source_set.remove(source)
+                    del classes[source]
+                    
+                    for key,val in re_direct.items():
+                        if val == source:
+                            re_direct[key]=re_direct[target]
 
-        weights = sorted([one for one in E.values() if one > 0])
-        threshold = weights[int(len(weights)*self.alpha)]
-
-        for key, value in E.items():
-            i, j = key[0], key[1]
-            if value <= threshold:
-                E[(i, j)], E[(j, i)] = 0, 0
-        return E_raw, E
+        return E_final,classes
+    
+    def topograph_construction_pruning(self,V,X_extend,Boundary,Dis,target):
+        E_raw = self.graph_construction(V,X_extend,Boundary,Dis)
+        E_final,node2cls = self.graph_pruning(E_raw,V,target,X_extend.shape[0])
+        return E_raw,E_final,node2cls

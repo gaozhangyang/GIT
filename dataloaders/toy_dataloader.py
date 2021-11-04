ee1a383dabffcd0cead1c775337d560b78b2ec01
chengtan <tancheng@westlake.edu.cn>
2021-11-04 08:40:15 +0000

    clean install

--- dataloaders/toy_dataloader.py
+++ dataloaders/toy_dataloader.py
@@ -2,7 +2,8 @@ import pandas as pd
 import os.path as osp
 
 
-toy_datasets = ['circles', 'impossible', 'moons', 's-set', 'smile']
+toy_datasets = ['circles', 'impossible', 'moons', 's-set', \
+                'smile', 'complex8', 'complex9', 'chainlink']
 
 class Toy_DataLoader:
     def __init__(self, name, path='./datasets/toy_datasets'):
@@ -12,6 +13,8 @@ class Toy_DataLoader:
 
     def load(self):
         df = pd.read_csv(osp.join(self.path, self.name + '.csv'), header=None)
-        X = df.values[:, :2]
-        Y_true = df.iloc[:, -1]
+        if self.name == 'chainlink':
+            X, Y_true = df.values[:,:3], df.iloc[:,-1]
+        else:
+            X, Y_true = df.values[:, :2], df.iloc[:, -1]
         return X, Y_true
--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from setuptools import setup
+from distutils.core import setup
+from distutils.command.clean import clean
+from distutils.command.install import install
 
 requirements = [
     'scikit-learn==0.23.2',
     'numpy==1.20.0',
     'scipy==1.7.1'
 ]
+
+
+class CleanInstall(install):
+    # Calls the default run command, then deletes the build area
+    # (equivalent to "setup clean --all").
+    def run(self):
+        install.run(self)
+        c = clean(self.distribution)
+        c.all = True
+        c.finalize_options()
+        c.run()
+
 
 setup(
     name="git_cluster",
@@ -13,5 +27,7 @@ setup(
     author_email="gaozhangyang@westlake.edu.cn",
     description="GIT: Clustering Based on Graph of Intensity Topology",
     packages=['git_cluster'],
-    install_requires=requirements
+    install_requires=requirements,
+    # egg_base='/tmp',
+    cmdclass={'install': CleanInstall}
 )
